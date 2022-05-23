c389795ac865405a77bbe2699447d33eac31fb6d
Sander Opperud Odden <sander.opperud.odden@nav.no>
2022-05-23 14:53:10 +0200

    Histogram antall dager

--- src/main/kotlin/no/nav/helse/prosessering/v1/PreprossesertSøknadMetrics.kt
+++ src/main/kotlin/no/nav/helse/prosessering/v1/PreprossesertSøknadMetrics.kt
@@ -1,6 +1,7 @@
 package no.nav.helse.prosessering.v1
 
 import io.prometheus.client.Counter
+import io.prometheus.client.Histogram
 import no.nav.helse.prosessering.v1.søknad.PreprosessertSøknad
 
 private val generelCounter = Counter.build()
@@ -9,33 +10,43 @@ private val generelCounter = Counter.build()
     .labelNames("spm", "svar")
     .register()
 
+val antallDagerHistogram = Histogram.build()
+    .name("antall_dager_histogram")
+    .register()
+
 internal fun PreprosessertSøknad.reportMetrics(){
     utenlandsoppholdIPeriodenMetrikk()
     pleietrengendeMetrikk()
     opptjeningIUtlandet()
     verneplikt()
+    søknadsperiode()
 }
 
-internal fun PreprosessertSøknad.pleietrengendeMetrikk(){
+private fun PreprosessertSøknad.søknadsperiode() {
+    val antallDagerIPerioden = fraOgMed.datesUntil(tilOgMed.plusDays(1)).count().toDouble()
+    antallDagerHistogram.observe(antallDagerIPerioden)
+}
+
+private fun PreprosessertSøknad.pleietrengendeMetrikk(){
     if(pleietrengende.norskIdentitetsnummer == null){
         generelCounter.labels("pleietrengendeUtenFnr", "ja").inc()
         generelCounter.labels("pleietrengendeUtenFnrGrunn", "${pleietrengende.årsakManglerIdentitetsnummer}").inc()
     } else generelCounter.labels("pleietrengendeUtenFnr", "nei").inc()
 }
 
-internal fun PreprosessertSøknad.utenlandsoppholdIPeriodenMetrikk(){
+private fun PreprosessertSøknad.utenlandsoppholdIPeriodenMetrikk(){
     if(utenlandsoppholdIPerioden.skalOppholdeSegIUtlandetIPerioden == true){
         generelCounter.labels("utenlandsoppholdIPerioden", "ja").inc()
     } else generelCounter.labels("utenlandsoppholdIPerioden", "nei").inc()
 }
 
-internal fun PreprosessertSøknad.opptjeningIUtlandet(){
+private fun PreprosessertSøknad.opptjeningIUtlandet(){
     if(this.opptjeningIUtlandet.isNotEmpty()){
         generelCounter.labels("opptjeningIUtlandet", "ja").inc()
     } else generelCounter.labels("opptjeningIUtlandet", "nei").inc()
 }
 
-internal fun PreprosessertSøknad.verneplikt(){
+private fun PreprosessertSøknad.verneplikt(){
     if(this.harVærtEllerErVernepliktig == true){
         generelCounter.labels("verneplikt", "ja").inc()
     } else generelCounter.labels("verneplikt", "nei").inc()
